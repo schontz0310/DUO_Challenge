09280f720b162f962e495b477c0bb62fd4fed5b4
schontz0310 <schontz0310@gmail.com>
2020-10-16 13:33:30 -0300

    newextras

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable no-unused-expressions */
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Alert } from 'react-native';
 import { Brightness, Invert } from 'react-native-color-matrix-image-filters';
 import { Form } from '@unform/mobile';
@@ -13,22 +13,39 @@ import Logo from '../../assets/logo.png';
 
 import { Container, LogoBox, Title } from './styles';
 
+interface FormDataProps {
+  email: string;
+  name: string;
+}
+
 const Register: React.FC = () => {
+  const [FormData, setFormData] = useState<FormDataProps>({
+    name: 'a',
+    email: 'a',
+  });
   const formref = useRef<FormHandles>(null);
 
-  const handleRegister = useCallback(data => {
-    Alert.alert(
-      'Cadastro Concluido',
-      `Parabéns ${data.name}, você receberá nossas noticias no email ${data.email}`,
-      [
-        {
-          text: 'OK',
-          onPress: () => {},
-        },
-      ],
-      { cancelable: false },
-    );
-  }, []);
+  const handleRegister = useCallback(
+    (data: FormDataProps) => {
+      setFormData({
+        name: data.name,
+        email: data.email,
+      });
+      console.log(FormData);
+      Alert.alert(
+        'Cadastro Concluido',
+        `Parabéns ${data.name}, você receberá nossas noticias no email ${data.email}`,
+        [
+          {
+            text: 'OK',
+            onPress: () => {},
+          },
+        ],
+        { cancelable: false },
+      );
+    },
+    [FormData],
+  );
 
   return (
     <Container>
